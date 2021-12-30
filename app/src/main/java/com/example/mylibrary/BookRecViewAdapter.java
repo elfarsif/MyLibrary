596419944a94c90040385aea02feec6e009921eb
Frank Norris El Farsi <f.n.elfarsi@gmail.com>
2021-12-30 15:55:18 -0500

    See all books has card View now

--- app/src/main/java/com/example/mylibrary/BookRecViewAdapter.java
+++ app/src/main/java/com/example/mylibrary/BookRecViewAdapter.java
@@ -2,11 +2,13 @@ package com.example.mylibrary;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -53,6 +55,19 @@ public class BookRecViewAdapter extends RecyclerView.Adapter<BookRecViewAdapter.
                 Toast.makeText(mContext,books.get(position).getName()+" Selected", Toast.LENGTH_SHORT).show();
             }
         });
+
+        holder.txtAuthor.setText(books.get(position).getAuthor());
+        holder.txtDescription.setText(books.get(position).getShortDesc());
+
+        if (books.get(position).isExpanded()){
+            TransitionManager.beginDelayedTransition(holder.parent);
+            holder.expandedRelLayout.setVisibility(View.VISIBLE);
+            holder.downArrow.setVisibility(View.GONE);
+        }else {
+            TransitionManager.beginDelayedTransition(holder.parent);
+            holder.expandedRelLayout.setVisibility(View.GONE);
+            holder.downArrow.setVisibility(View.VISIBLE);
+        }
         
     }
 
@@ -72,12 +87,41 @@ public class BookRecViewAdapter extends RecyclerView.Adapter<BookRecViewAdapter.
         private ImageView imgBook;
         private TextView txtName;
 
+        private ImageView downArrow, upArrow;
+        private RelativeLayout expandedRelLayout;
+        private TextView txtAuthor,txtDescription;
+
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             parent=itemView.findViewById(R.id.parent);
             imgBook=itemView.findViewById(R.id.imgBook);
             txtName=itemView.findViewById(R.id.txtBookName);
 
+
+            downArrow=itemView.findViewById(R.id.btnDownArrow);
+            upArrow=itemView.findViewById(R.id.btnUpArrow);
+            expandedRelLayout=itemView.findViewById(R.id.expandedRelLayout);
+            txtAuthor=itemView.findViewById(R.id.txtAuthor);
+            txtDescription=itemView.findViewById(R.id.shortDescText);
+
+            downArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Book book = books.get(getAdapterPosition());
+                    book.setExpanded(!book.isExpanded());
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
+
+            upArrow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Book book = books.get(getAdapterPosition());
+                    book.setExpanded(!book.isExpanded());
+                    notifyItemChanged(getAdapterPosition());
+                }
+            });
         }
     }
 }
